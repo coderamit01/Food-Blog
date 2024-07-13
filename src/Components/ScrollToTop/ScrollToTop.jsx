987c8074045cbea5a8e66242c2d9e3eca6abd428
amit <amitmohammad46@gmail.com>
2024-07-14 00:11:17 +0600

    fix scroll issue

--- src/Components/ScrollToTop/ScrollToTop.jsx
+++ src/Components/ScrollToTop/ScrollToTop.jsx
@@ -1,14 +1,12 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLayoutEffect } from "react";
+import { useLocation } from "react-router-dom";
+
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo({ top:0, left:0, behavior:"instant"})
+  },[location.pathname])
 };
 
 export default ScrollToTop;
